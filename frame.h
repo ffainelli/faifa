aa65fd1c74a261fcec8089d6bc5673425bff6f4a
Xavier Carcelle <xarcelle@gmail.com>
2009-09-07 11:16:13 +0000

    Adding LICENSE.OpenSSL and copyrights exception in the source code due to the usage of the OpenSSL library

--- frame.h
+++ frame.h
@@ -20,6 +20,21 @@
  *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
  */
 
+/*
+ *  In addition, as a special exception, the copyright holders give
+ *  permission to link the code of portions of this program with the
+ *  OpenSSL library under certain conditions as described in each
+ *  individual source file, and distribute linked combinations
+ *  including the two.
+ *  You must obey the GNU General Public License in all respects
+ *  for all of the code used other than OpenSSL.  If you modify
+ *  file(s) with this exception, you may extend this exception to your
+ *  version of the file(s), but you are not obligated to do so.  If you
+ *  do not wish to do so, delete this exception statement from your
+ *  version.  If you delete this exception statement from all source
+ *  files in the program, then also delete it here.
+ */
+
 int ether_init_header(void *buf, int len, u_int8_t *da, u_int8_t *sa, u_int16_t ethertype);
 int set_init_callback(u_int16_t mmtype, int (*callback)(void *buf, int len, void *user));
 int set_dump_callback(u_int16_t mmtype, int (*callback)(void *buf, int len, struct ether_header *hdr));
